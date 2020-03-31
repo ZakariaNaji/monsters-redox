80477a27387f17084b4ffea1d0b33dd6fd79c35d
znaji <znaji@sqli.com>
2020-03-31 17:20:54 +0100

    Updates

---
+++ precache-manifest.71cc4a20465c679a740942a42a0c4a7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "148410cfe688ea1b2e9aa0d4a17fc2c1",
+    "url": "/monsters-redox/index.html"
+  },
+  {
+    "revision": "a6f9d3d4e97c417d7b48",
+    "url": "/monsters-redox/static/css/main.c3e72582.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/monsters-redox/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-redox/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6f9d3d4e97c417d7b48",
+    "url": "/monsters-redox/static/js/main.38868256.chunk.js"
+  },
+  {
+    "revision": "3a38c9770bc1262c43f2",
+    "url": "/monsters-redox/static/js/runtime-main.6ba2168b.js"
+  }
+]);
